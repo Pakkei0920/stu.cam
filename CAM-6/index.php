a5fc853ce065921d65590110916717d8b0004bc6
Pak Kei <106027537+Pakkei0920@users.noreply.github.com>
2023-11-11 21:43:26 +0800

    Update index.php

--- CAM-6/index.php
+++ CAM-6/index.php
@@ -1,4 +1,6 @@
 <?php
+    unlink("output_20231111.mp4");
+
     $imageDirectory = __DIR__ . '/';
 
     // Check if form is submitted
@@ -23,17 +25,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.plyr.io/3.6.4/plyr.css" />
-    <title>Plyr.io Example with Subtitles</title>
+    <title>STU CAM-6 | 監控回放</title>
 </head>
 <body>
 
 <!-- Form to input date variable -->
 <form method="post">
-    <label for="dateVariable">Enter Date Variable (e.g., 20231101): </label>
+    <label for="dateVariable">請輸入日期變數(e.g. 20231101): </label>
     <input type="text" id="dateVariable" name="dateVariable" required>
-    <button type="submit">Generate Video</button>
+    <button type="submit">確認</button>
 </form>
 
+    <p> 監控接口調用需時 | 耐心等待 | 如長時間無反應可 F5或刷新網站</p>
 <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
     <!-- Display video if form is submitted -->
     <video id="player" controls>
